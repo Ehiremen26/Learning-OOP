a3ce9c231ecde0d491cf720bf5e5a2b2caa62fc6
Ogieriakhi Ehiremen Franklyn <franklyn.ogieriakhi@bmedsci.uniben.edu>
2024-06-20 22:33:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// creating a constructor function
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  // console.log(this);
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // adding methods (Never do this)
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3.{} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
+
+// Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+
+console.log(jonas.__proto__);
